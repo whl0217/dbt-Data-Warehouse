cfcafbea7a8766d9b61b5d7314fd6d00d5b0f6b6
Wang-Han Li <98363049+whl0217@users.noreply.github.com>
2024-03-02 21:06:24 -0500

    Add files via upload

---
+++ models/marts/int_order_items.sql
@@ -0,0 +1,16 @@
+select
+    line_items.order_item_key,
+    line_items.part_key,
+    line_items.line_number,
+    line_items.extended_price,
+    orders.order_key,
+    orders.customer_key,
+    orders.order_date,
+    {{discounted_amount('line_items.extended_price', 'line_items.discount_percentage')}} as item_discount_amount
+from
+    {{ ref('stg_tpch_orders')}} as orders
+join
+    {{ ref('stg_tpch_line_items')}} as line_items
+        on orders.order_key = line_items.order_key
+order by
+    orders.order_date
